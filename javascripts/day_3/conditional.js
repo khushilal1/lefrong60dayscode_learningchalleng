ed170351a9193eb3429a95f4250fa1b74c8fd5bc
khushilal1 <singh.khushilal1119@gmail.com>
2022-04-17 23:09:13 +0545

    added files

---
+++ javascripts/day_3/conditional.js
@@ -0,0 +1,53 @@
+// tring contenation using the + operator
+// awe use the (+) operator to concatenate strings
+let result="my"+"string";
+console.log(result);//this will print (my stri)ng because the + operator concatenates strings withou the gapping
+
+// concatenation with gapping be  as
+// let result="my"+"string";
+let result_1="my"+" "+"string"
+console.log(result_1);//this will print (my string)
+
+// cocatenating of the number be as
+console.log(1+2);//this will print 3
+
+console.log(1+"2");//this will print 12s
+
+console.log(1+2+"2");//this will print 32
+
+console.log("2"+"3"+1+2+3);//this will print 23123 but not 236
+
+
+
+// mathematical operation using the String
+// concatenation
+console.log(6-"4");//this will print 2  because the first is number so it terat alll other as number from the string
+
+console.log("4"-3)//this will print 1
+
+// the i nterseting examples as
+
+
+let a=0
+console.log(Boolean(a));//this will print false
+
+let b="0"
+console.log(Boolean(b));//this will print trueS
+console.log(a==b);//this will print true
+
+// strict equality operator
+
+// the symbol of  strict equality operator be ===
+// it can differentiate the  0 and false but the normal == doest not/
+console.log(0===false);//this will print false\)    
+// if statemenr\
+
+//ELSE STATEMENT
+
+let year=prompt("enter your birhday year",[2013]);
+year=Number(year);
+if(year==2013){
+    console.log("you are a baby");
+}
+
+
