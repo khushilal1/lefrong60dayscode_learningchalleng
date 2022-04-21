34ca48f749c60eb24f1f83f3af044fb00d828234
khushilal1 <singh.khushilal1119@gmail.com>
2022-04-21 22:59:57 +0545

    adeed files

---
+++ javascripts/day_7/arrow_function/arrow.js
@@ -0,0 +1,14 @@
+console.log("arrow function");
+// there are two  type arrow function thta singlee and multi arrow function
+
+ sum=(a,b)=>a+b;
+    console.log(sum(10,20));
+
+
+    // multi line arrow function
+let  age=53
+let welcome=(age>23)?
+()=>console.log("hello"):
+()=>console.log("welcome");
+
+welcome();
---
+++ javascripts/day_7/objects/object.js
@@ -0,0 +1,63 @@
+
+// object are the same as arrays, but they are objects.
+// object are the king of arrays
+
+// every data type are the object except the primitive value
+// the primitive value are the number, string, boolean, null, undefined, symbol
+// string
+// number
+// boolean
+// null
+// undefined
+// symbol
+// bigint
+
+// thhe primitve value are im mitauble which cannot be cahbge
+
+////object are also variable
+// the object are written in key valu pair
+
+
+// Objects written as name value pairs are similar to:
+
+// Associative arrays in PHP
+// Dictionaries in Python
+// Hash tables in C
+// Hash maps in Java
+// Hashes in Ruby and Perl
+
+// Creating a JavaScript Object
+
+// Using an Object Literal
+
+let object={age:20, name:'John'};
+    console.log(object);//gettin the valu of the objrcct on the console
+    console.log(object.name);//getting the value of the age
+
+
+
+    // Using the JavaScript Keyword new
+let person=new object();
+    console.log(person);//gettin the valu of the objrcct on the console
+    person.first="molhan"
+    person.last="yadzv"
+
+    console.log()
+
+    // Using the JavaScript Keyword new
+
+    const car = {type:"Fiat", model:"500", color:"white"};
+console,lo
+
+
+// It is a common practice to declare objects with the const keyword.
+
+// Learn more about using const with objects in the chapter: JS Const.
+
+
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    eyeColor: "blue"
+  };
