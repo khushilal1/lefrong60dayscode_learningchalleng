15350996d4cbb762dfd415ac1956789fd38bcaf1
khushilal1 <singh.khushilal1119@gmail.com>
2022-04-27 23:51:08 +0545

    added the math object file

---
+++ javascripts/day_13/math/math.js
@@ -0,0 +1,30 @@
+// this object is used to perform the math operatoiin
+console.log(`the value of pi be :${Math.PI}`)
+
+console.log(Math.round(546.65465)); //it rund off the nmber increese by 1 whch same as the mathemaical opeartion
+console.log(Math.round(546.25465));
+
+console.log(Math.pow(2,3)); //it gives 8
+
+// same as
+console.log(2**3);
+console.log(Math.pow(3,2));//ity gives 9
+console.log(Math.sqrt(100));//it give sthe square root of the nmber
+console.log((Math.abs(-455))); //it gives same as the absolute value in mathematics
+console.log((Math.abs(-455.756)));
+//ceil function used to up inceraes by 1
+console.log(Math.ceil(99.01)); //it gives 100
+console.log(Math.round(99.01));//it follwos ame as round numv=ber rule as 99
+
+console.log(Math.floor(88.956));//it fives the nearest value at lower point
+console.log(Math.max(46,536,-56));//it give sthe max number
+console.log(Math.min(46,536,-56));//it give sthe min number
+
+
+//////////////////////////////////
+// console.log(Math.random(3,4,5,6,79,3)*10);
+console.log(Math.floor(Math.random(3,4,5,6,79,3)*10));
+///////////////////
+// Math.trunc() return the interhger part of a number
+console.log(Math.trunc(657.767));
+console.log(Math.trunc(-657.77));
