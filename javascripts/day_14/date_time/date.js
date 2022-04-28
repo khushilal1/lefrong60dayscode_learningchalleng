09a35f066f8f0da04faf0defeeb607def1b3d0ef
khushilal1 <singh.khushilal1119@gmail.com>
2022-04-28 22:03:35 +0545

    added files

--- javascripts/day_14/date_time/date.js
+++ javascripts/day_14/date_time/date.js
@@ -5,13 +5,13 @@
 // that reprent since 1 january 1970 
 
 
-// let currdDate=new Date();
-// console.log(currdDate)
+let currdDate=new Date();
+console.log(currdDate)
 // // ****************************************
 // // in the local method
-// console.log(currdDate.toLocaleString())//give in the formate  26/04/2022, 20:58:31
-// console.log(currdDate.toString())//give in the formate  26/04/2022, 20:58:31
-//   console.log(Date.now()); //return the millisecond
+console.log(currdDate.toLocaleString())//give in the formate  26/04/2022, 20:58:31
+console.log(currdDate.toString())//give in the formate  26/04/2022, 20:58:31
+  console.log(Date.now()); //return the millisecond
   
 
 
@@ -22,17 +22,17 @@
 
 
 //   *******************************
-// var date=new Date(2022,0,34)
-// console.log(date.toLocaleString())
+var date=new Date(2022,0,34)
+console.log(date.toLocaleString())
 
 
 
 // date onl
-// const currDate=new Date()
-// console.log(currDate.toLocaleString())
-// console.log(currDate.getFullYear())
-// console.log(currDate.getMonth());///0=>jan to 11=>dece
-// console.log(currDate.getDate());
+const currDate=new Date()
+console.log(currDate.toLocaleString())
+console.log(currDate.getFullYear())
+console.log(currDate.getMonth());///0=>jan to 11=>dece
+console.log(currDate.getDate());
 
 
 // Set method //to ste time manualy
@@ -40,24 +40,24 @@
 // console.log(currDate.setDate(2022,0));
 
 // time returns
-// const currtime=new Date()
-// console.log(currtime.getTime());
-// console.log(currtime.getDay());  //day interms of no. that is as 1=>mon,2=>tue,3=>wed,4=>thur,5=>fri,6=>sat,7=>sa
-// console.log(currtime.getHours());
-// console.log(currtime.getMinutes());
-// console.log(currtime.getSeconds());
-// console.log(currtime.getMilliseconds());
+const currtime=new Date()
+console.log(currtime.getTime());
+console.log(currtime.getDay());  //day interms of no. that is as 1=>mon,2=>tue,3=>wed,4=>thur,5=>fri,6=>sat,7=>sa
+console.log(currtime.getHours());
+console.log(currtime.getMinutes());
+console.log(currtime.getSeconds());
+console.log(currtime.getMilliseconds());
 
 // set TimeRanges
 // console.log(currtime.setTime());
 // ***************************************************
 // set meyhod
 
-// function date(){
-//     var currtime=new Date()
-// currtime.setDate(5)
-//     document.getElementById("demo").innerHTML=currtime;
-// }
+function date(){
+    var currtime=new Date()
+currtime.setDate(5)
+    document.getElementById("demo").innerHTML=currtime;
+}
 
 
 
